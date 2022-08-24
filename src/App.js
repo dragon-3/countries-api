2935d580977c8b6d4eb5f185fa1e13df85c94f04
Sean Narron <seannarron9@gmail.com>
2022-08-24 17:51:44 +0900

    Added router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import CountryList from './CountryList';
+import Edit from './Edit';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
+
     <div className="App">
-      <CountryList />
+    <Router>
+
+      <Routes>
+      <Route path="/" element={<CountryList />} />
+      <Route path=":id" element={<Edit />} />
+      </Routes>
+    </Router>
+
     </div>
   );
 }
